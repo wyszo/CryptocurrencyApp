ec256ee9b8d3d07c5c5b5c14680d15bd80aee260
Thomas W <thomas@wyszomirski.co.uk>
2020-12-23 20:52:34 +0000

    Cryptocurrencies JSON and a model file added

---
+++ CryptocurrencyApp/CryptocurrencyApp/Model/Cryptocurrency.swift
@@ -0,0 +1,26 @@
+//
+//  Cryptocurrency.swift
+//  CryptocurrencyApp
+//
+//  Copyright © 2020 Thomas Wyszomirski. All rights reserved.
+//
+
+import Foundation
+
+struct Cryptocurrency: Codable, Equatable {
+    let name: String
+    let shortName: String
+    let priceUSD: Double
+    let sevenDaysChangePercent: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case shortName
+        case priceUSD
+        case sevenDaysChangePercent = "7dChangePercent"
+    }
+}
+
+struct CryptocurrenciesList {
+    let marketCapBilionsUSD: Double
+}
