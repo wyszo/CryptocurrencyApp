2d356ac3d7fb30293e4ca80c59c249179f486ea4
Thomas W <thomas@wyszomirski.co.uk>
2021-01-01 21:57:00 +0000

    Cryptocurrency Detail View layout updated. StackViews added.

--- CryptocurrencyApp/CryptocurrencyApp/Controllers/CryptocurrencyDetail/CryptocurrencyDetailViewController.swift
+++ CryptocurrencyApp/CryptocurrencyApp/Controllers/CryptocurrencyDetail/CryptocurrencyDetailViewController.swift
@@ -6,7 +6,12 @@
 //
 
 import UIKit
+import Resolver
 
 class CryptocurrencyDetailViewController: UIViewController {
     
+    @IBOutlet private weak var name: UILabel!
+    @IBOutlet private weak var shortName: UILabel!
+    @IBOutlet private weak var price: UILabel!
+    @IBOutlet private weak var change7d: UILabel!
 }
